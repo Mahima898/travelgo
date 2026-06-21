5b916c089e708f41f5504f90004882329bf70913
Mahima <mahima@example.com>
2026-06-22 01:38:58 +0530

    Add Vercel URL to CORS

--- travel-go-backend/main.py
+++ travel-go-backend/main.py
@@ -37,6 +37,7 @@ app.add_middleware(
         "http://localhost:5184",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+         "https://travelgo-beta-azure.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
